8aaee7ca565909ed8ed20942d036b42655c2dca9
Adi052447 <adi.gamzu@gmail.com>
2024-12-26 10:03:02 +0200

    aaa

--- gym/management/Sessions/SessionFactory.java
+++ gym/management/Sessions/SessionFactory.java
@@ -7,13 +7,13 @@ public class SessionFactory {
     // מתודה ליצירת שיעור חדש בהתבסס על סוג השיעור
     public static Session createSession(SessionType sessionType, String dateTime, ForumType forumType, Instructor instructor) {
         switch (sessionType) {
-            case SessionType.Pilates:
+            case Pilates:
                 return new PilatesSession(dateTime, forumType, instructor);
-            case SessionType.ThaiBoxing:
+            case ThaiBoxing:
                 return new ThaiBoxingSession(dateTime, forumType, instructor);
-            case SessionType.MachinePilates:
+            case MachinePilates:
                 return new MachinePilatesSession(dateTime, forumType, instructor);
-            case SessionType.Ninja:
+            case Ninja:
                 return new NinjaSession(dateTime, forumType, instructor);
             default:
                 throw new IllegalArgumentException("Unknown session type: " + sessionType);
